6894fa80017af65c72b2378d513917c06a85d3f7
sheetal <sheetalkumarilife@gmail.com>
2024-09-11 16:51:58 +0530

    updated ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   async function handle(e) {
     e.preventDefault();
     try {
+    
       const response = await axios.post("https://nodemailer-0v80.onrender.com/sendEmail", {
         name,
         email,
@@ -24,23 +25,26 @@ function App() {
   }
 
   return (
-    <>
+    <div className=" w-full h-[100vh]  flex flex-col justify-center items-center">
     {dataSaved ? <p>Email Sent</p> :""}
-      <h1>Nodemailer App</h1>
-      <form action="" onSubmit={handle}>
+      <h1 className="font-bold text-4xl">Nodemailer App</h1>
+      <form action="" onSubmit={handle} className="border-2 border-gray-400 p-4 flex flex-col justify- m-4 text-xl rounded-2xl shadow-xl	shadow-gray-500">
         <input
+        className="border-2 border-gray-600 p-2 m-2"
           type="text"
           placeholder="Enter your name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
         <input
+        className="border-2 border-gray-600 p-2 m-2"
           type="text"
           placeholder="Enter your email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <textarea
+        className="border-2 border-gray-600 p-2 m-2"
           name=""
           value={message}
           id=""
@@ -50,9 +54,9 @@ function App() {
           {message}
         </textarea>
 
-        <button type="submit">Send Email</button>
+        <button type="submit" className="bg-blue-800 text-white text-2xl p-2 my-4 rounded-2xl">SEND EMAIL</button>
       </form>
-    </>
+    </div>
   );
 }
 
