563d3e9b8281ae86a497395c923a6c980acf1210
sheetal <sheetalkumarilife@gmail.com>
2024-09-11 23:15:55 +0530

    fixed bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,30 +5,33 @@ function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const[dataSaved, setDataSaved] = useState(false);
+  const[send,setSend] = useState(false)
 
   async function handle(e) {
     e.preventDefault();
     try {
-    
-      const response = await axios.post("https://nodemailer-0v80.onrender.com/sendEmail", {
+    // https://nodemailer-0v80.onrender.com/
+      const response = await axios.post("http://localhost:6969/sendEmail", {
         name,
         email,
         message,
       });
-      if (response.status === 200 && response.statusText === "Ok") {
-        setDataSaved(!dataSaved);
-      }
-      setDataSaved(!dataSaved);
+      console.log(response)
 
+      if(response.status == 200 && response.statusText == 'OK'){
+          setSend(true);
+      }
     } catch (e) {
       console.log(e);
     }
   }
 
   return (
+
     <div className=" w-full h-[100vh]  flex flex-col justify-center items-center">
-    {dataSaved ? <p>Email Sent</p> :""}
+{send &&
+      <h1 className="text-4xl font-semibold text-green-500">Email Send Successfully !!</h1>
+     }
       <h1 className="font-bold text-4xl">Nodemailer App</h1>
       <form action="" onSubmit={handle} className="border-2 border-gray-400 p-4 flex flex-col justify- m-4 text-xl rounded-2xl shadow-xl	shadow-gray-500">
         <input
@@ -40,7 +43,7 @@ function App() {
         />
         <input
         className="border-2 border-gray-600 p-2 m-2"
-          type="text"
+          type="email"
           placeholder="Enter your email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
